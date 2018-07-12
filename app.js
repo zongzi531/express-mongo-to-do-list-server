6e1e9128c74c144e2f1db4358e21edcc02246a87
zongzi531 <zongzi.xy@gmail.com>
2018-07-12 20:41:28 +0800

    Add morgan and js-base64 library

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 
 const router = require('./routes')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('combined'))
 
 app.use(router)
 
